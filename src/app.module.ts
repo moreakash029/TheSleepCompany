e788e0cd12a958b5fd179070a42e38ae336e9e56
Nitesh Mourya <niteshmourya009@gmail.com>
2026-01-07 14:02:14 +0530

    Rate_Limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HealthController } from './health/health.controller';
+
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [
+    TerminusModule,
+    HttpModule,
+
+    // Rate limiting configuration (official syntax)
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000, // 60 seconds (milliseconds as per docs)
+          limit: 10, // max 100 requests
+        },
+      ],
+    }),
+  ],
   controllers: [AppController, HealthController],
-  providers: [AppService],
+  providers: [
+    AppService,
+
+    // lobal guard binding (REQUIRED in v6)
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './instrumentation';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Important for correct IP detection behind proxies
+  app.set('trust proxy', true);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
